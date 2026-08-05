9dd70ee79dc51971ccacd314e39e340fce9c69b8
FadllanCamp4 <FadllanCamp4>
2026-08-05 13:40:35 +0700

    Menambahkan soal 5

---
+++ Function/SoalFunction5.js
@@ -0,0 +1,12 @@
+// Tulis function displayUserInfo di sini
+function displayUserInfo(nama, umur, kota) {
+    console.log("=== Info User ===");
+    console.log("Nama: " + nama);
+    console.log("Umur: " + umur + " tahun");
+    console.log("Kota: " + kota);
+    console.log("");
+}
+
+// Panggil function dengan data berbeda
+displayUserInfo("Ahmad", 25, "Jakarta");
+displayUserInfo("Lisa", 30, "Bandung");
