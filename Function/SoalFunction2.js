a2523772bda071a40a138358e20be67c7180b9a0
FadllanCamp4 <FadllanCamp4>
2026-08-05 13:29:41 +0700

    Menambahkan soal 2

---
+++ Function/SoalFunction2.js
@@ -0,0 +1,14 @@
+// Tulis function getCurrentTime di sini
+function getCurrentTime() {
+    let sekarang = new Date();
+
+    let jam = String(sekarang.getHours()).padStart(2, "0");
+    let menit = String(sekarang.getMinutes()).padStart(2, "0");
+    let detik = String(sekarang.getSeconds()).padStart(2, "0");
+
+    return jam + ":" + menit + ":" + detik;
+}
+
+// Panggil function dan simpan hasilnya
+let waktu = getCurrentTime();
+console.log("Waktu sekarang: " + waktu);
