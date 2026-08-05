be3185817df593d010512a8b50e27c3338f52cdc
FadllanCamp4 <FadllanCamp4>
2026-08-05 14:17:41 +0700

    Menambahkan soal 7

---
+++ Function/SoalFunction7.js
@@ -0,0 +1,11 @@
+// Tulis function createProfile di sini
+function createProfile(nama, status = "Member") {
+    return "Profil: " + nama + " (" + status + ")";
+}
+
+// Panggil function dengan dan tanpa parameter kedua
+let profil1 = createProfile("Budi");
+let profil2 = createProfile("Sari", "Admin");
+
+console.log(profil1);
+console.log(profil2);
