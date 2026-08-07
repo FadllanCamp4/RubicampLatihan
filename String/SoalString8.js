d4ca19331d277cccea07553e78b9ef00115d941d
FadllanCamp4 <fadllanramadhan3@gmail.com>
2026-08-07 15:13:19 +0700

    Test

--- String/SoalString8.js
+++ String/SoalString8.js
@@ -23,6 +23,7 @@ const cleanAndFormat = (text, width) => {
     };
 };
 
+
 console.log(cleanAndFormat("  Hello   World  ", 15));
 console.log(cleanAndFormat("JavaScript", 12));
 console.log(cleanAndFormat("  Code  ", 8));
